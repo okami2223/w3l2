9fd19cc6b8809ad18f323fae449efe892121e853
okami <luookami@gmail.com>
2019-07-20 14:58:17 +0800

    控制滑動旋轉

--- w3l2/ViewController.swift
+++ w3l2/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var timer:Timer!
+    var counter = 0.0
+    var speed = 2.0
+    @IBOutlet weak var segment: UISegmentedControl!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let newView = UIView(frame: CGRect(x: 0, y:0, width: 100,height: 100))
+        newView.center = self.view.center
+        newView.backgroundColor = UIColor.red
+        newView.transform = CGAffineTransform(rotationAngle: 0)
+        view.addSubview(newView)
+        timer = Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true, block: { (t) in
+            self.rotateView(targateView: newView)
+        })
     }
-
-
+    func rotateView(targateView:UIView){
+        let angle = counter * Double.pi / 180
+        targateView.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
+        counter += speed
+        
+        
+    }
+    
+    @IBAction func segmentAction(_ sender: UISegmentedControl) {
+        
+    }
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        print(sender.value)
+        speed = Double(sender.value)
+    }
+    
 }
-
